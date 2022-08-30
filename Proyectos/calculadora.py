23c34794bb1736f02a9bcc496881f229c59665fd
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2022-08-30 16:52:00 -0300

    calculadora

--- Proyectos/calculadora.py
+++ Proyectos/calculadora.py
@@ -1,4 +1,26 @@
 # Calculadora en Python
+from os import system
+from datetime import date
+import random
+import platform
+autor = "Joao Affonso"
+
+so = platform.system()
+if so == "Linux":
+    s = "clear"
+else:
+    s = "cls"
+
+hoy = date.today()
+dia = hoy.day
+mes = hoy.month
+anio = hoy.year
+
+system(s)
+
+print("Autor: "+f" {autor}".rjust(100, "-"))
+print("Fecha: "+f" {dia}/{mes}/{anio}".rjust(100, "-"))
+
 n1 = float(input('Escriba el primer número: '))
 op = str(input('Qué operación matemática desea realizar? '))
 n2 = float(input('Escriba el segundo número: '))
