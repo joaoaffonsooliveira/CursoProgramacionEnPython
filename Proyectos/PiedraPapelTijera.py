c270d2094bfe1f03fbd46fe16a9edfb92bd576f4
João Affonso Oliveira Alves de Araujo <affonso.oliver@gmail.com>
2022-08-30 02:08:17 -0300

    Mudanzas en PiedraPapelTijera

--- Proyectos/PiedraPapelTijera.py
+++ Proyectos/PiedraPapelTijera.py
@@ -1,17 +1,22 @@
 # Juego Piedra Papel Tijera
-
 import random
 # Unicode Emojis: op[0] es piedra op[1] es papel y op[2] es tijera
 op = ["\U0000270A", "\U0001F91A", "\U0000270C"]
-j1 = random.choice(op)
+e1 = int(input('Vamos jugar Jo Ken Po!\nEscriba uno de los numeros abajo para jugar: \n1 para elegir "\U0000270A" \n2 para elegir "\U0001F91A" \n3 para elegir "\U0000270C"\n'))
+if e1 == 1:
+    j1 = op[0]
+elif e1 == 2:
+    j1 = op[1]
+elif e1 == 3:
+    j1 = op[2]
 j2 = random.choice(op)
 
 if (j1 == op[0] and j2 == op[2]) or (j1 == op[2] and j2 == op[1]) or (j1 == op[1] and j2 == op[0]):
-    print(f'Jugador 1: {j1}\n    X \nJugador 2: {j2}  \n{"-"*15}\nJugador 1 ha ganado!!')
+    print(f'Eligiste: {j1}\n    X \nJugador 2 ha elegido: {j2}  \n{"-"*15}\nGanaste!!')
 elif (j1 == op[0] and j2 == op[0]) or (j1 == op[1] and j2 == op[1]) or (j1 == op[2] and j2 == op[2]):
-    print(f'Jugador 1: {j1}\n    X \nJugador 2: {j2}  \n{"-"*15}\nLos jugadores han empatado')
+    print(f'Eligiste: {j1}\n    X \nJugador 2 ha elegido: {j2}  \n{"-"*15}\nLos jugadores han empatado')
 else:
-    print(f'Jugador 1: {j1}\n    X \nJugador 2: {j2}  \n{"-"*15}\nJugador 1 ha perdido!!')
+    print(f'Eligiste: {j1}\n    X \nJugador 2 ha elegido: {j2}  \n{"-"*15}\nPerdiste!!')
 
 # Abajo hay una variación del ejercicio anterior usando el modulo emoji
 """
